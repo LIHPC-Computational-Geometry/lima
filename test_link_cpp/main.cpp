d8423857d48d9c4be6ea689b1023e374c36ae348
franck.ledoux <franck.ledoux@cea.fr>
2023-01-08 12:51:32 +0100

    - test cpp file renaming

--- test_link_cpp/main.cpp
+++ test_link_cpp/main.cpp
@@ -1,13 +1,9 @@
-#include <Lima/malipp.h>
-#include <Lima/erreur.h>
-#include <Lima/polyedre.h>
 #include "Lima/lima++.h"
 #include <Lima/malipp2.h>
 
 int main(){
     double lenght_unit = 1.;
     Lima::MaliPPWriter2* writer=new Lima::MaliPPWriter2("toto", 1);
-
     writer->unite_longueur(lenght_unit);
     writer->dimension(Lima::D2);
     writer->close();
