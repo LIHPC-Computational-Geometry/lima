d74ad1c890529b4492686f634cfd11ca64fc1aec
franck.ledoux <franck.ledoux@cea.fr>
2023-01-08 12:57:38 +0100

    - chage the used lima writer

--- test_link_cpp/main.cpp
+++ test_link_cpp/main.cpp
@@ -1,9 +1,9 @@
 #include "Lima/lima++.h"
-#include <Lima/malipp2.h>
+#include <Lima/malipp.h>
 
 int main(){
     double lenght_unit = 1.;
-    Lima::MaliPPWriter2* writer=new Lima::MaliPPWriter2("toto", 1);
+    Lima::MaliPPWriter* writer=new Lima::MaliPPWriter("toto", 1);
     writer->unite_longueur(lenght_unit);
     writer->dimension(Lima::D2);
     writer->close();
